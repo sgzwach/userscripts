2322da7e75cc6505160c3363ae4e70b4bb94a152
shawn <sgzwach@gmail.com>
2020-07-28 12:12:42 -0500

    Trying to fix install links...

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ These are scripts that make my day to day a bit better. Most of them are not wel
 Banner has awful formatting by default. This script adjusts the formatting so class info spans multiple lines, but doesn't use double line breaks. Additionally, a show all
 option is added to the pagination drop down. Around 150 classes will make your browser upset.
 
-* [Install Banner Usability Fixes](https://raw.github.com/sgzwach/userscripts/master/Banner_BrowseClasses_Style.js)
+* [Install Banner Usability Fixes](https://raw.githubusercontent.com/sgzwach/userscripts/master/banner-browseclasses.js)
---
+++ banner-browseclasses.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name           Banner Styling
+// @namespace      http://tampermonkey.net/
+// @version        0.2
+// @description    List all classes and format a touch better
+// @author         shawn 
+// @match          https://registration.sdbor.edu/StudentRegistrationSsb/ssb/classSearch/classSearch
+// @require        https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant          GM_addStyle
+// @run-at         document-end
+// @updateURL      https://raw.githubusercontent.com/sgzwach/userscripts/master/banner-browseclasses.js
+// @downloadURL    https://raw.githubusercontent.com/sgzwach/userscripts/master/banner-browseclasses.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_addStyle('.tooltip-row { display: block; }');
+    waitForKeyElements('td', function (node) {
+        if (node.attr("data-property") && node.data("property") == "attribute") {
+            node.html(node.html().replace(/<br><br>/g, "<br>"));
+        }
+    });
+    waitForKeyElements('.page-size-select', function(node) {
+        node.append(new Option("All",$(".results-out-of").text().split(" ")[0]));
+    });
+})();
