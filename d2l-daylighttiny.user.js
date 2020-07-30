e8dcb7f116e57cbff9cc5a8c948115eb65a5c294
sgzwach <sgzwach@users.noreply.github.com>
2020-07-30 15:39:42 -0500

    Update d2l-daylighttiny.user.js
    Exclude grading page

--- d2l-daylighttiny.user.js
+++ d2l-daylighttiny.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name           D2L Daylight Tiny
 // @namespace      http://github.com/sgzwach
-// @version        0.1
+// @version        0.2
 // @description    Shrink the daylight theme where appropriate.
 // @author         Shawn
 // @match          https://d2l.sdbor.edu/d2l*
+// @exclude        https://d2l.sdbor.edu/d2l/lms/dropbox/admin/mark/folder_user_mark.d2l*
 // @grant          none
 // @updateURL      https://raw.githubusercontent.com/sgzwach/userscripts/master/d2l-daylighttiny.user.js
 // @downloadURL    https://raw.githubusercontent.com/sgzwach/userscripts/master/d2l-daylighttiny.user.js
