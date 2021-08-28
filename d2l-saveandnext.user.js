6ce6857ec0e929eee88585c916bdd85531b1e177
sgzwach <sgzwach@users.noreply.github.com>
2021-08-28 13:44:00 -0500

    Updated for new grading workflow

--- d2l-saveandnext.user.js
+++ d2l-saveandnext.user.js
@@ -1,53 +1,26 @@
 // ==UserScript==
 // @name           Autosave Grade Updates on D2L
 // @namespace      http://github.com/sgzwach
-// @version        0.8
+// @version        0.9
 // @description    Saves grade changes within D2L automatically when "Next student" is clicked in the dropbox. Also, autoopen the first doc.
 // @author         Shawn
-// @match          https://d2l.sdbor.edu/d2l/lms/dropbox/admin/mark/folder_user_mark.d2l*
+// @match          https://d2l.sdbor.edu/d2l/le/activities/iterator/*
 // @grant          none
 // @updateURL      https://raw.githubusercontent.com/sgzwach/userscripts/master/d2l-saveandnext.user.js
 // @downloadURL    https://raw.githubusercontent.com/sgzwach/userscripts/master/d2l-saveandnext.user.js
-
-// @run-at         document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Function to click save if found
-    // mostly because the anonymous functions they use to do so are a pain to deal with
-    function clickSaveButton() {
-        var buttons = $('.d2l-button'); // jquery is required within d2l - use their version to prevent breaking feedback box
-        for (var i=0; i < buttons.length; i++)
-            if (buttons[i].innerText == 'Save')
-                buttons[i].click();
+    function inTheShadows() {
+        var tokenReceiver = document.querySelector('.d2l-token-receiver');
+        var saveButton = tokenReceiver.shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('d2l-template-primary-secondary').querySelector('d2l-consistent-evaluation-footer-presentational').shadowRoot.querySelector('#consistent-evaluation-footer-save-draft').shadowRoot.querySelector('.d2l-label-text');
+        var nextButton = tokenReceiver.shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('d2l-template-primary-secondary').querySelector('d2l-consistent-evaluation-footer-presentational').shadowRoot.querySelector('#consistent-evaluation-footer-next-student').shadowRoot.querySelector('[title="Next Student"]');
+        var topNextButton = tokenReceiver.shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('d2l-template-primary-secondary').querySelector('d2l-consistent-evaluation-nav-bar').shadowRoot.querySelector('d2l-navigation-iterator').shadowRoot.querySelector('d2l-navigation-iterator-item').shadowRoot.querySelector('d2l-navigation-button').shadowRoot.querySelector('.d2l-focusable');
+        nextButton.addEventListener("click", function(){saveButton.click();});
     }
 
-    // find div with next student link and add eventListener to click the save button
-    var divs = $('.dco_c');
-    var done = false;
-    for (var i=0; i < divs.length && !done; i++) {
-        for (var c=0; c < divs[i].children.length && !done; c++) {
-            if (divs[i].children[c].title == "Next Student" || divs[i].children[c].title == "Next Group") {
-                divs[i].children[c].addEventListener("click", clickSaveButton); // modified after talking to Mike to just use next student button - Mike also found this didn't work for groups
-                done = true;
-            }
-        }
-    }
-
-    // second pass at auto opening document
-    var docs = $('.dfl');
-    done = false;
-    var link = null;
-    for (i=0; i < docs.length && !done; i++)
-    {
-        link = docs[i].children[0];
-        var ll = link.title.toLowerCase();
-        if ((ll.endsWith("pdf") || ll.endsWith("docx") || ll.endsWith("doc") || ll.endsWith("rtf") || ll.endsWith('odt') || ll.endsWith('ppt') || ll.endsWith('pptx') || ll.endsWith("png")) && link.href.startsWith("javascript"))
-            done = true;
-    }
-    if (done)
-        setTimeout(function() {link.click();}, 1250);
+    setTimeout(inTheShadows, 2000);
 
 })();
