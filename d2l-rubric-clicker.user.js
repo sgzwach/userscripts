3c7740f2ab8ee0c5a3290204ba4c20114324d7b2
sgzwach <sgzwach@users.noreply.github.com>
2021-09-19 18:14:31 -0500

    Update d2l-rubric-clicker.user.js

--- d2l-rubric-clicker.user.js
+++ d2l-rubric-clicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Default Rubric Settings
 // @namespace      http://github.com/sgzwach
-// @version        0.1
+// @version        0.2
 // @description    Set all rubric criteria to 100% on page load and only remove points
 // @author         Shawn
 // @match          https://d2l.sdbor.edu/d2l/le/activities/iterator/*
@@ -16,6 +16,9 @@
     function rubricMark() {
         var tokenReceiver = document.querySelector('.d2l-token-receiver');
         var rubricGroups = tokenReceiver.shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('d2l-template-primary-secondary').querySelector('consistent-evaluation-right-panel').shadowRoot.querySelector('d2l-consistent-evaluation-rubric').shadowRoot.querySelector('d2l-consistent-evaluation-right-panel-block').querySelector('d2l-rubric').shadowRoot.querySelector('d2l-rubric-adapter').querySelector('d2l-rubric-criteria-groups').shadowRoot.querySelectorAll('d2l-rubric-criteria-group-mobile');
+        var rubricPanel = tokenReceiver.shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('d2l-template-primary-secondary').querySelector('consistent-evaluation-right-panel').shadowRoot.querySelector('d2l-consistent-evaluation-rubric').shadowRoot.querySelector('d2l-consistent-evaluation-right-panel-block').querySelector('d2l-rubric').shadowRoot.querySelector('d2l-rubric-adapter').shadowRoot.querySelector('d2l-labs-accordion-collapse').shadowRoot.querySelector('#trigger');
+        console.log(rubricPanel);
+        rubricPanel.click();
         console.log(rubricGroups);
 
         for (var i = 0; i < rubricGroups.length; i++) {
