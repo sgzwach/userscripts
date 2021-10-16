4311302bbf48b43c89685227474fd0d70c035770
sgzwach <sgzwach@users.noreply.github.com>
2021-10-16 14:16:45 -0500

    Added mutation observer for opening docs

--- d2l-saveandnext.user.js
+++ d2l-saveandnext.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Autosave Grade Updates on D2L
 // @namespace      http://github.com/sgzwach
-// @version        1.1
+// @version        1.2
 // @description    Saves grade changes within D2L automatically when "Next student" is clicked in the dropbox. Also, autoopen the first doc.
 // @author         Shawn
 // @match          https://d2l.sdbor.edu/d2l/le/activities/iterator/*
@@ -23,6 +23,19 @@
         nextButton.addEventListener("click", function(){saveButton.click();});
     }
 
+    function openDocument() {
+        var documentButton = document.querySelector('.d2l-token-receiver').shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('d2l-template-primary-secondary').querySelector('d2l-consistent-evaluation-left-panel').shadowRoot.querySelector('d2l-consistent-evaluation-evidence-assignment').shadowRoot.querySelector('d2l-consistent-evaluation-submissions-page').shadowRoot.querySelector('d2l-consistent-evaluation-submission-item').shadowRoot.querySelector('.truncate');
+        documentButton.click();
+    }
+
     setTimeout(inTheShadows, 3000);
+
+    new MutationObserver(function(mutations) {
+        if (mutations.length >= 1)
+            setTimeout(openDocument, 1000);
+    }).observe(
+        document.querySelector('title'),
+        { subtree: true, characterData: true, childList: true }
+    );
 
 })();
