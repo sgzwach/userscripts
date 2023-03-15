fac00e809521460e478f47f6b6ce45ef20787f08
Tyler <tjflaagan@users.noreply.github.com>
2023-03-15 16:51:10 -0500

    Updating version TF

--- adfs-realmhint.user.js
+++ adfs-realmhint.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           RealmHintMagic
 // @namespace      http://github.com/sgzwach
-// @version        0.4
+// @version        0.5
 // @description    Auto fill DSU realm hint
 // @author         shawn
 // @match          https://adfs.sdbor.edu/adfs/ls*
