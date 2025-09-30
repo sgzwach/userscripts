d28d12d492fc31beb83f694bc66981e8cf05fdbc
sgzwach <sgzwach@users.noreply.github.com>
2025-09-30 15:58:13 -0500

    Update d2l-rubric-clicker.user.js
    Shout out to Kyle K for the notes!

--- d2l-rubric-clicker.user.js
+++ d2l-rubric-clicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Default Rubric Settings
 // @namespace      http://github.com/sgzwach
-// @version        0.5
+// @version        0.7
 // @description    Set all rubric criteria to 100% on page load and only remove points
 // @author         Shawn
 // @match          https://d2l.sdbor.edu/d2l/le/activities/iterator/*
@@ -13,75 +13,24 @@
 (function() {
     'use strict';
 
-    var lock = false;
-    var rubriclock = false;
-    var retries = 0;
-    function clickCriterion() {
-        if (lock || retries > 100) {
-            return;
-        }
-        lock = true;
-        var md = new Event("mousedown", {bubbles: true});
-        var retries = 0;
-
-        // wrap this entire disastor in a try catch loop - will spin up to 100 times
-        retries += 1;
-        try {
-            // iterate over groups, finding left arrows and clicking as required
-            //var groups = document.querySelector('d2l-consistent-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('#evaluation-template').querySelector('consistent-evaluation-right-panel').shadowRoot.querySelector('.d2l-consistent-evaluation-right-panel').querySelector('d2l-consistent-evaluation-rubric').shadowRoot.querySelector('d2l-consistent-evaluation-right-panel-block').querySelector('d2l-rubric').shadowRoot.querySelector('d2l-rubric-adapter').querySelector('d2l-rubric-criteria-groups').shadowRoot.querySelectorAll('d2l-rubric-criteria-group-mobile');
-            var groups = document.querySelector('d2l-consistent-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('consistent-evaluation-right-panel').shadowRoot.querySelector('consistent-evaluation-right-panel-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-right-panel-rubric').shadowRoot.querySelector('d2l-rubric').shadowRoot.querySelector('d2l-rubric-adapter').querySelector('d2l-rubric-criteria-groups').shadowRoot.querySelectorAll('d2l-rubric-criteria-group-mobile');
-            console.log("Groups", groups);
-            if (groups.length == 0)
-                throw new Error('no groups defined');
-            var i, j;
-            for (i = 0; i < groups.length; i++) {
-                // in each group, look at each criterion
-                var criterion = groups[i].shadowRoot.querySelectorAll('d2l-rubric-criterion-mobile');
-                console.log("Criterion", criterion);
-                if (criterion.length == 0)
-                    throw new Error('no criterion defined');
-                for (j = 0; j < criterion.length; j++) {
-                    // check if a level is NOT selected
-                    console.log(criterion[j].shadowRoot.querySelector('d2l-rubric-levels-mobile'));
-                    console.log(criterion[j].shadowRoot.querySelector('d2l-rubric-levels-mobile').shadowRoot.querySelectorAll('div [aria-checked="true"]'))
-                    // if there are no aria attributes, defer again
-                    if (criterion[j].shadowRoot.querySelector('d2l-rubric-levels-mobile').shadowRoot.querySelectorAll('div [aria-checked]').length == 0)
-                        throw new Error('no radio buttons defined');
-                    if (criterion[j].shadowRoot.querySelector('d2l-rubric-levels-mobile').shadowRoot.querySelectorAll('div [aria-checked="true"]').length == 0) {
-                        // click the left arrow to select the high level
-                        criterion[j].shadowRoot.querySelector('d2l-rubric-levels-mobile').shadowRoot.querySelector('#left-iterator').dispatchEvent(md);
-                    }
-                }
-            }
-        } catch (err) {
-            console.log("retrying...", err);
-            setTimeout(clickCriterion, 1000);
-        }
-        lock = false;
-        rubriclock = false;
-        retries = 0;
-    }
-
-
     function rubricMark() {
-        var panel = document.querySelector('d2l-consistent-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('consistent-evaluation-right-panel').shadowRoot.querySelector('consistent-evaluation-right-panel-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-right-panel-rubric').shadowRoot.querySelector('d2l-rubric').shadowRoot.querySelector('d2l-rubric-adapter').shadowRoot.querySelector('d2l-collapsible-panel').shadowRoot.querySelector('d2l-icon-custom');
-        if (panel == null) {
-            console.log("panel still loading...");
-            setTimeout(function(){rubricMark();}, 200);
-            return;
-        }
-        console.log("Panel", panel);
-        if (!rubriclock) {
-            rubriclock = true;
-            var cl = new Event("click", {bubbles: true});
-            panel.dispatchEvent(cl);
-            setTimeout(function(){clickCriterion()}, 1000);
-        }
+        // Thanks to Kyle K for identifying the new elipsis menu that allows "Set All" options!
+        // As of 10/2025 tab index 0 is the default left orrientation
+        // Tyler orrientation, or right most, is the FIRST tab index -1 in the list.
+        // Find, click, bubble!
+        var btn = document.querySelector('d2l-consistent-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('consistent-evaluation-right-panel').shadowRoot.querySelector('consistent-evaluation-right-panel-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-right-panel-rubric').shadowRoot.querySelector('d2l-rubric').shadowRoot.querySelector('d2l-rubric-adapter').shadowRoot.querySelector('d2l-rubric-select-all-dropdown').shadowRoot.querySelector('d2l-menu').querySelector('[tabindex="0"]');
+
+
+        // Tyler orrientation below
+        // var btn = document.querySelector('d2l-consistent-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('consistent-evaluation-right-panel').shadowRoot.querySelector('consistent-evaluation-right-panel-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-right-panel-rubric').shadowRoot.querySelector('d2l-rubric').shadowRoot.querySelector('d2l-rubric-adapter').shadowRoot.querySelector('d2l-rubric-select-all-dropdown').shadowRoot.querySelector('d2l-menu').querySelector('[tabindex="-1"]');
+        btn.click();
+
+        // enable or disable opening rubric here. Default is to open; comment to disable.
+        var rubricdrop = document.querySelector('d2l-consistent-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('consistent-evaluation-right-panel').shadowRoot.querySelector('consistent-evaluation-right-panel-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-right-panel-rubric').shadowRoot.querySelector('d2l-rubric').shadowRoot.querySelector('d2l-rubric-adapter').shadowRoot.querySelector('d2l-collapsible-panel').shadowRoot.querySelector('d2l-icon-custom.d2l-skeletize');
+        rubricdrop.click()
     }
 
-    var mo_title = new MutationObserver(function(mutations){
-        console.log("TITLE MUTATION!!!!");
-        console.log(mutations);
+    var mo_username = new MutationObserver(function(mutations){
         if (mutations.length >= 1) {
             setTimeout(rubricMark, 1000);
         }
@@ -90,16 +39,13 @@
     // monitor body mutations until title is added, then use that to trigger rubric fill on new student
     var mo_body = new MutationObserver(function(mutations) {
         if (mutations.length >= 1) {
-            //console.log(mutations);
             for (var i in mutations) {
                 if (mutations[i].type == "childList" && mutations[i].addedNodes.length >= 1) {
-                    //console.log(mutations[i].addedNodes);
-                    // once title is created, mutate on that and kill this one
+                    // once title is created, mutate on username changes and disable this observer
                     for (var j = 0; j < mutations[i].addedNodes.length; j++) {
-                        console.log("Checking ", mutations[i].addedNodes[j]);
                         if (mutations[i].addedNodes[j].localName == "title") {
-                            mo_title.observe(document.querySelector("title"), {characterData: true, subtree: true, childList: true});
-                            rubricMark();
+                            mo_username.observe(document.querySelector('d2l-consistent-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('d2l-consistent-evaluation-learner-context-bar').shadowRoot.querySelector('d2l-consistent-evaluation-lcb-user-context').shadowRoot.querySelector('.d2l-consistent-evaluation-lcb-user-name'), {characterData: true, subtree: true, childList: true});
+                            setTimeout(rubricMark, 1000);
                             this.disconnect();
                         }
                     }
