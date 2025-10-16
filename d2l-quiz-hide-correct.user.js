ca695ab54e09d45b8a35ac7c7936e3acde5cf228
sgzwach <sgzwach@users.noreply.github.com>
2025-10-16 11:41:14 -0500

    Update user script to version 0.6
    Updated for new unified scoring view on D2L.

--- d2l-quiz-hide-correct.user.js
+++ d2l-quiz-hide-correct.user.js
@@ -1,78 +1,101 @@
 // ==UserScript==
 // @name         D2L Skip Questions Graded Correct in Quizzes
 // @namespace    http://github.com/sgzwach
-// @version      0.5
+// @version      0.6
 // @description  Any question marked correct should be minimized automatically, but can be expanded again
 // @author       shawn
-// @match        https://d2l.sdbor.edu/d2l/lms/quizzing/admin/mark/quiz_mark_attempt.d2l*
+// @match        https://d2l.sdbor.edu/d2l/le/activities/iterator/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sdbor.edu
 // @updateURL    https://raw.githubusercontent.com/sgzwach/userscripts/master/d2l-quiz-hide-correct.user.js
 // @downloadURL  https://raw.githubusercontent.com/sgzwach/userscripts/master/d2l-quiz-hide-correct.user.js
 // @grant        none
 // ==/UserScript==
 
+// Much of this code is copied from the Rubric Mark script as the necessary steps to detect SPA view changes are included already
 (function() {
     'use strict';
 
-    function toggleQuestion(e) {
-        // check the text content of the caller (lazy, but probably effective enough)
-        var t = e.target;
-        var nt = t;
+    // Gemini wrote this part lol
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
-        // handle modified questions
-        if (t.closest('div').nextElementSibling.innerText.includes("This question is an old version")) {
-            nt = t.closest('div').nextElementSibling.nextElementSibling;
-        }
-        else {
-            nt = t.closest('div').nextElementSibling;
-        }
-        if (t.innerText == '+') {
-            // expand!
-            nt.hidden = false;
-            t.innerText = '-';
+    function toggleQuestion(e) {
+        var sibling = e.target.nextElementSibling; // this should be the heading of a question
+        if (sibling.hidden) {
+            sibling.hidden = false;
         } else {
-            // contract!
-            nt.hidden = true;
-            t.innerText = '+';
+            sibling.hidden = true;
         }
     }
 
-    function hideCorrect() {
-        var i, divToHide, questionInfo, middleColumn;
-
-        // Add handler for toggling question to any div that has one
-        var middleColumns = document.getElementsByClassName('dlay_m');
-        for (i = 0; i < middleColumns.length; i++) {
-            middleColumns[i].addEventListener("mouseup", toggleQuestion);
-            middleColumns[i].style.cursor = 'pointer';
-            middleColumns[i].innerText = '-';
+    async function hideQuestions() {
+        console.log("Woot!");
+        var questions = document.querySelector("d2l-consistent-evaluation").shadowRoot.querySelector("d2l-consistent-evaluation-page").shadowRoot.querySelector("d2l-consistent-evaluation-left-panel").shadowRoot.querySelector("d2l-consistent-evaluation-evidence-quizzing").shadowRoot.querySelector("d2l-consistent-evaluation-quizzing-attempt").shadowRoot.querySelectorAll("d2l-consistent-evaluation-quizzing-attempt-result");
+        var scores = [];
+        var i, score, gradeType, header;
+        var n, d;
+        // loop until all scores are added; we don't really use this other than to determine that the view is ready to continue
+        while (scores.length < questions.length) {
+            for (i = 0; i < questions.length; i++) {
+                // for each question, find the score
+                score = questions[i].shadowRoot.querySelector("d2l-labs-grade-result-presentational");
+                if (score && !scores.includes(score))
+                    scores.push(score);
+            }
+            if (scores.length < questions.length) {
+                console.log(scores.length);
+                await sleep(250); // added this to avoid locking up the page
+            }
         }
 
-        // Find any "Correct response" alt text
-        //var correct = document.querySelectorAll('d2l-icon[alt="Correct Response"]');
-        var correct = document.querySelectorAll('[label="Score"]');
-        if (correct.length > 0 && typeof correct[0].value == "undefined")
-            setTimeout(hideCorrect, 1000);
-        else
-        {
-
-            // hide any question that is correct
-            for (i = 0; i < correct.length; i++) {
-                if (correct[i].value >= 1) {
-                    divToHide = correct[i].closest('div');
-                    questionInfo = divToHide.previousElementSibling;
-                    // handle case where question has been modified
-                    if (questionInfo.innerText.includes("This question is an old version")) {
-                        questionInfo = questionInfo.previousElementSibling;
-                    }
-                    divToHide.hidden = true;
-                    questionInfo.querySelector('.dlay_m').innerText = '+';
+        // once we know the scores are all visible, iterate over the questions again to hide those at 100%
+        for (i = 0; i < questions.length; i++) {
+            score = questions[i].shadowRoot.querySelector("d2l-labs-grade-result-presentational");
+            gradeType = score.getAttribute("grade-type");
+            // For every question, add an event listener to toggle visibility
+            header = questions[i].shadowRoot.querySelector(".d2l-consistent-eval-quiz-question-header");
+            header.addEventListener("mouseup", toggleQuestion);
+            header.style.cursor = "pointer";
+            if (gradeType == "Numeric") { // we have a number! Check the numerator and denominator
+                n = score.getAttribute("score-numerator");
+                d = score.getAttribute("score-denominator");
+                if (n >= d)
+                {
+                    questions[i].shadowRoot.querySelector(".d2l-consistent-eval-quiz-question-wrapper").hidden = true;
                 }
             }
         }
     }
 
-    hideCorrect();
+    var mo_username = new MutationObserver(function(mutations){
+        if (mutations.length >= 1) {
+            setTimeout(hideQuestions, 1000);
+        }
+    });
 
+    // monitor body mutations until title is added, then use that to trigger rubric fill on new student
+    var mo_body = new MutationObserver(function(mutations) {
+        if (mutations.length >= 1) {
+            for (var i in mutations) {
+                if (mutations[i].type == "childList" && mutations[i].addedNodes.length >= 1) {
+                    // once title is created, mutate on username changes and disable this observer
+                    for (var j = 0; j < mutations[i].addedNodes.length; j++) {
+                        if (mutations[i].addedNodes[j].localName == "title") {
+                            if (mutations[i].addedNodes[j].innerText.substring(0,4) == "Quiz") { // make sure title includes Quiz, else exit
+                                mo_username.observe(document.querySelector('d2l-consistent-evaluation').shadowRoot.querySelector('d2l-consistent-evaluation-page').shadowRoot.querySelector('d2l-consistent-evaluation-learner-context-bar').shadowRoot.querySelector('d2l-consistent-evaluation-lcb-user-context').shadowRoot.querySelector('.d2l-consistent-evaluation-lcb-user-name'), {characterData: true, subtree: true, childList: true});
+                                setTimeout(hideQuestions, 1000);
+                            }
+                            this.disconnect();
+                        }
+                    }
+                }
+            }
+        }
+    }).observe(
+        document,
+        { subtree: true, characterData: true, childList: true }
+    );
 
 })();
+
